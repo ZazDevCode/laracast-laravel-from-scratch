f19ef5517324cf8ecb881830289bf5e24749e81a
Sascha Wernars <s@scha.nl>
2022-07-21 00:06:21 +0200

    Finished Episode 41: Author Filtering

--- resources/views/components/post-featured-card.blade.php
+++ resources/views/components/post-featured-card.blade.php
@@ -43,7 +43,6 @@
                               <h5 class="font-bold">
                                 <a href="/authors/{{$post->author->username}}"> {{ $post->author->name }}</a>
                               </h5>
-                              <h6>Mascot at Laracasts</h6>
                           </div>
                       </div>
 
