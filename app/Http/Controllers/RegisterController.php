089539ff872539a0cdf5218cf260108a4ba03b93
Sascha Wernars <s@scha.nl>
2022-08-04 19:13:48 +0200

    Finished Episode 48: Show a Success Flash Message

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Validation\Rule;
 
 class RegisterController extends Controller
 {
@@ -14,13 +15,14 @@ class RegisterController extends Controller
     public function store(){   
         $attributes = request()->validate([
             'name' => ['required','max:255'],
-            'username' => ['required','max:255','min:3'],
-            'email' => ['required','email','max:255'],
+            'username' => ['required','min:3','max:255', Rule::unique('users','username')],
+            'email' => ['required','email','max:255', Rule::unique('users','email')],
             'password' => ['required','min:7'],
         ]);
 
         User::create($attributes);
-        return redirect('/');
+        // session()->flash('success', 'Your account has been created.');
+        return redirect('/')->with('success', 'Your account has been created.');;
     }
     
     
