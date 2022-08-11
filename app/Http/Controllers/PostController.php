e76bd90107a8da17d691c0ee4d23d25e922de1d7
Sascha Wernars <s@scha.nl>
2022-08-11 17:59:15 +0200

    Finished Episode 63: Create Publish Post Form

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Post;
 use App\Models\Category;
-use Illuminate\Http\Request;
+use Illuminate\Validation\Rule;
 
 class PostController extends Controller
 {
@@ -18,6 +18,26 @@ class PostController extends Controller
 
     public function show(Post $post){
         return view('posts.show', ['post' => $post]);
-    }    
+    }
+
+    public function create(){
+        return view('posts.create');
+    } 
+
+    public function store(){
+        $attributes = request()->validate([
+            'title' => ['required'],
+            'slug' => ['required', Rule::unique('posts','slug')],
+            'excerpt' => ['required'],
+            'body' => ['required'],
+            'category_id' => ['required', Rule::exists('categories','id')]
+        ]);
+
+        $attributes['user_id'] = auth()->id();
+
+        Post::create($attributes);
+        return redirect('/')->with('success', 'Your succesfully submitted a post.');
+    }
+    
 }
  
